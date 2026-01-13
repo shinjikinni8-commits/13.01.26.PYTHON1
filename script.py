09cf6886df4fc4bf01319a0ae9180601d54ef2d5
aaa <aaa.gmail.com>
2026-01-13 23:11:52 +0200

    poj"M:

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ name = "Ann"
 age = 16
 city = "Odessa"
 role = "student"
+college = "IT Step college"
 
-print('My name is ', name, '. I am ', age, '. I am from', city, ' and my role is', role, '.')
-print('My name is ' + name + '. I am ' + str(age) + '. I am from ' + str(city) + 'and my role is' + str(role) + '.')
-print('My name is {} . I am {} . I am from {} and my role is {}.'.format(name, age, city, role))
-print(f'My name is {name}. I am {age}. I am from {city} and my role is {role}.')
+print('My name is ', name, '. I am ', age, '. I am from', city, ' and my role is', role, ', I study in the', college,'.')
+print('My name is ' + name + '. I am ' + str(age) + '. I am from ' + str(city) + ' and my role is ' + str(role) + ', I study in the ' + str(college) + '.')
+print('My name is {} . I am {} . I am from {} and my role is {}, I study int the {}.'.format(name, age, city, role, college))
+print(f'My name is {name}. I am {age}. I am from {city} and my role is {role}, I study int the {college}.')
