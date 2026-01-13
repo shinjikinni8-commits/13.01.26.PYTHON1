8d2867fbd4de2f5b636b1ffe8396500ec2a69db7
aaa <aaa.gmail.com>
2026-01-13 22:16:43 +0200

    щтж

--- script.py
+++ script.py
@@ -1,17 +1,9 @@
-print("Hello World!")
-name = input("Enter ur name: ")
-print("Hello! My name's " + name)
-name = 'Ann'
+name = "Ann"
 age = 16
-height = 1.65
-is_student = True
+city = "Odessa"
+role = "student"
 
-print(name, age, height, is_student)
-print(name + ' ' + str(age) + ' ' + str(height) + ' ' + str(is_student))
-print('{} {} {} {}'.format(name, age, height, is_student))
-print(f'{name} {age} {height} {is_student}')
-
-print('My name is ', name, '. I am ', age, '. My height is', height, ' and my status is', is_student, ' (student).')
-print('My name is ' + name + '. I am ' + str(age) + '. My height is' + str(height) + 'and my status is' + str(is_student) + ' (student).')
-print('My name is {} . I am {} . My height is {} and my status is {} (student).'.format(name, age, height, is_student))
-print(f'My name is {name}. I am {age}. My height is {height} and my status is {is_student} (student).')
+print('My name is ', name, '. I am ', age, '. I am from', city, ' and my role is', role, '.')
+print('My name is ' + name + '. I am ' + str(age) + '. I am from ' + str(city) + 'and my role is' + str(role) + '.')
+print('My name is {} . I am {} . I am from {} and my role is {}.'.format(name, age, city, role))
+print(f'My name is {name}. I am {age}. I am from {city} and my role is {role}.')
